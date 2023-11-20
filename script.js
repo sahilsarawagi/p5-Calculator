cdbc598d492d9d5215f2567994d43ab0c1f76d16
sahil sarawagi <sahilsarawagi1@gmail.com>
2023-11-21 00:24:58 +0530

    -Add <p> tag for number display -Implement logic for displaying calculations and answers.

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ let operator=null;
 const numbers = document.querySelectorAll('.numbers');
 const operators = document.querySelectorAll('.operator');
 const equlaSign =document.querySelector('.equalSign');
+const paraCalculationDisplay = document.querySelector('.calculationDisplay');
+const paraAnswerDisplay = document.querySelector('.answerDisplay');
+
+
+function calculationDisplay(string,addingString){
+    console.log(`inside of para  ${string}`)
+    if(addingString){
+        paraCalculationDisplay.textContent+=string;
+    }
+    else{
+        paraCalculationDisplay.textContent=string;
+    }
+}
+
+function updatelastIndexDisplayCalculation(string){
+    para = paraCalculationDisplay.textContent;
+    paraCalculationDisplay.textContent = para.slice(0,-1) + string;
+}
+
+function answerDisplay(string){
+    paraAnswerDisplay.textContent=string;
+}
 
 
 // this function prints the value of variables
@@ -30,25 +52,32 @@ numbers.forEach((button) => {
 function handleNumberButtonLogic(buttonValue){
     if(!num1 && !operator){ 
         num1 = buttonValue;
-        console.log(`num1  ${num1}`)
+        calculationDisplay(num1,false);
+        console.log(`num1  ${num1}`);
     }
     else if(!operator){
         if(num1==='0'){
          num1=buttonValue;
+         calculationDisplay(num1,false);
         }else{
         num1 += buttonValue;
-        console.log(`num1  ${num1}`)
+        calculationDisplay(num1,false);
+        answerDisplay('');
+        console.log(`num1  ${num1}`);
     }
     }
     else if(!num2){
         num2 = buttonValue;
         console.log(`num2  ${num2}`)
+        calculationDisplay(num2,true);
     }else {
         if(num2==='0'){
             num2=buttonValue;
+            updatelastIndexDisplayCalculation(num2,true);
         }else{
         num2 += buttonValue;
         console.log(`num2  ${num2}`)
+        calculationDisplay(num2.charAt(num2.length-1),true);
         }
     }
 }
@@ -64,6 +93,7 @@ function handleEqualButtonLogic(){
         return
     }
     num1=calculation(num1,num2,operator);
+    answerDisplay(num1);
     num2=null;
     operator=null;
 }
@@ -71,7 +101,7 @@ function handleEqualButtonLogic(){
 // this function listens when user clicks  any operator button and it assign value to variable 'operator'
 operators.forEach((button) => {
     button.addEventListener('click', () => {   
-    console.log(`inside operator  ${typeof(button.value)}`)
+    console.log(`inside operator  ${typeof(button.value)}`);
     valueCheck();
     handleOperatorButtonLogic(button.value)
     valueCheck();
@@ -79,12 +109,27 @@ operators.forEach((button) => {
 });
 
 function handleOperatorButtonLogic(buttonValue){
+    if(!operator){
+        answerDisplay('');
+        calculationDisplay('',false);
+        calculationDisplay(num1,false);
+        calculationDisplay(buttonValue,true);
+}
+
     if(num2){
         num1 = calculation(num1,num2,operator);
+        answerDisplay(num1);
+        calculationDisplay('',false);
+        calculationDisplay(num1,false);
+        calculationDisplay(buttonValue,true);
+        operator = buttonValue;
         console.log(`num1  ${num1}`)
         num2=null;
     }
+    else{
     operator = buttonValue;
+    updatelastIndexDisplayCalculation(operator);
+    }
 
 }
 
